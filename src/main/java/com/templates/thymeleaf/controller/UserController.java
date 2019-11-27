bfcee10ae07df049bbcfbd0df1ead30ddda610ee
phayo <anazodogodwin@gmail.com>
2019-11-27 18:26:04 +0100

    add usercontroller and demo.html

---
+++ src/main/java/com/templates/thymeleaf/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.templates.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("demo")
+    public String demo(Model model) {
+        double grade = 96.5;
+        model.addAttribute("message", "Hello Ebuka");
+        model.addAttribute("grade", grade);
+        model.addAttribute("GPA", convertGPA(grade));
+        // return to templates/demo.html page.
+        return "demo";
+    }
+
+    private String convertGPA(double grade) {
+        if (grade >= 90) {
+            return "A";
+        } else if (grade < 90 && grade >= 80) {
+            return "B";
+        } else if (grade < 80 && grade >= 70) {
+            return "C";
+        } else if (grade < 70 && grade >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    @RequestMapping("demo3")
+    public String demo3(HttpServletRequest request, Model model) {
+        // Request
+        request.setAttribute("request", "request data");
+        // Session
+        request.getSession().setAttribute("session", "session data");
+        // Application
+        request.getSession().getServletContext().setAttribute("application", "application data");
+        return "demo";
+    }
+
+}
